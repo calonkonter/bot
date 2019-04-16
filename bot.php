54ea186817495dcec75c3b6c787c3cd61833aa2c
mrxnonameid86 <38692262+mrxnonameid86@users.noreply.github.com>
2019-04-17 00:42:37 +0700

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,17 @@
+<?php
+
+    $url = 'https://shortid.co/HGgVp';
+    
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookies.txt');
+    curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookies.txt');
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36');
+    $result = curl_exec($ch);
+
+    $ip=$_SERVER['REMOTE_ADDR'];
+    print "IP Address= $ip";
+
+?>
