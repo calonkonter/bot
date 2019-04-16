8a361e19ad9c60204f180f0594ad69eb8c8dd6b2
mrxnonameid86 <38692262+mrxnonameid86@users.noreply.github.com>
2019-04-17 01:55:00 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,8 +10,19 @@
     curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookies.txt');
     curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36');
     $result = curl_exec($ch);
+    curl_close($ch);
 
-    $ip=$_SERVER['REMOTE_ADDR'];
-    print "IP Address= $ip";
+    if(preg_match("/<a id=\"makingdifferenttimer\" style=\"display: none;\" rel=\"nofollow\" href=\"(.*?)\" title=\"Get Link\"\>/", $result, $links));
+    
+    $getLink = 'https://shortid.co'.$links[1];
+    
+    $ch = curl_init($getLink);
+    curl_setopt($ch, CURLOPT_URL, $getLink);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookies.txt');
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36');
+    $execute = curl_exec($ch);
+    curl_close($ch);
 
 ?>
